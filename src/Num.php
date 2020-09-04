0cae5543312147390ff1543ac5b71dd3ca0f1f56
Ivano Matteo <ivanomatteo@gmail.com>
2020-09-04 11:27:14 +0200

    parseInt

--- src/Num.php
+++ src/Num.php
@@ -26,32 +26,36 @@ class Num
     public static function parseInt($v, $accept_float = false, $accept_exp = false, $throwOnNull = true)
     {
         $n = null;
-        if (is_numeric($v)) {
-            $n = intval($v);
-            if (is_string($v)) {
-                if (!$accept_float && preg_match('/[.]/', $v)) {
-                    $n === null;
-                } else if (!$accept_exp && preg_match('/[eE]/', $v)) {
-                    $n === null;
-                } else {
-                    $v = trim($v);
 
-                    if ($n === 0 && $v != 0) {
-                        // piccolo trick, con stringhe numeriche molto grandi intval() restituisce 0
-                        // però $stringaNumerica == 0 --> false
-                        $n = null;
-                    } else if ($n === PHP_INT_MAX && $v !== ('' . PHP_INT_MAX)) {
-                        $n = null;
-                    } else if ($n === PHP_INT_MIN && $v !== ('' . PHP_INT_MIN)) {
-                        $n = null;
-                    }
-                }
-            } else if (is_float($v)) {
-                if ($accept_float) {
-                    if ($v > PHP_INT_MAX || $v < PHP_INT_MIN || $n === INF || $n === -INF || $n === NAN) {
-                        $n = null;
-                    }
+        if (is_int($v)) {
+            return $v;
+        } else if (is_float($v)) {
+            if ($accept_float) {
+                if ($v > PHP_INT_MAX || $v < PHP_INT_MIN || $n === INF || $n === -INF || $n === NAN) {
+                    $n = null;
                 } else {
+                    $n = intval($v);
+                }
+            } else {
+                $n = null;
+            }
+        } else if (is_numeric($v)) {
+            $n = intval($v);
+
+            if (!$accept_float && preg_match('/[.]/', $v)) {
+                $n === null;
+            } else if (!$accept_exp && preg_match('/[eE]/', $v)) {
+                $n === null;
+            } else {
+                $v = trim($v);
+
+                if ($n === 0 && $v != 0) {
+                    // piccolo trick, con stringhe numeriche molto grandi intval() restituisce 0
+                    // però $stringaNumerica == 0 --> false
+                    $n = null;
+                } else if ($n === PHP_INT_MAX && $v !== ('' . PHP_INT_MAX)) {
+                    $n = null;
+                } else if ($n === PHP_INT_MIN && $v !== ('' . PHP_INT_MIN)) {
                     $n = null;
                 }
             }
